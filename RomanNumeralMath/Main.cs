ec1361d315322386e549827f05a285e8db75f8d2
BrianTibbetts <boduntills@gmail.com>
2021-12-11 14:50:24 -0500

    Function Implementation
    Created RNumCharToInt function to convert each of the 7 roman numeral characters to an integer

---
+++ RomanNumeralMath/Main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace RomanNumeralMath
+{
+    public class Arithmetic
+    {
+        /* function RNumCharToInt - Converts 1 roman numeral character to an integer
+         * arguments
+         *  rnum, a roman numeral character
+         * return values
+         *  the integer corresponding to the roman numeral character's value
+         */
+        public int RNumCharToInt(char rnum)
+        {
+            switch(rnum)
+            {
+                case 'I':
+                    return 1;
+                case 'V':
+                    return 5;
+                case 'X':
+                    return 10;
+                case 'L':
+                    return 50;
+                case 'C':
+                    return 100;
+                case 'D':
+                    return 500;
+                case 'M':
+                    return 1000;
+                default:
+                    return 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Arithmetic arithmetic = new Arithmetic();
+            char rNum = 'V';
+            int convertedRNum = arithmetic.RNumCharToInt(rNum);
+            Console.WriteLine(rNum + " in integer form equals " + convertedRNum);
+        }
+    }
+}
